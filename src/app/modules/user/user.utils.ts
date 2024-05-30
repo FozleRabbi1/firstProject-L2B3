eeb1010fe7bf45c31730614ee34276103c9c084f
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-05-30 20:58:09 +0600

    update generateStudentId utils function

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,3 +1,28 @@
+// import { TAcademicSemester } from '../academicSemister/academicSemester.interface';
+// import { User } from './user.model';
+
+// const findLastStudenId = async () => {
+//   const lastStudent = await User.findOne(
+//     {
+//       role: 'student',
+//     },
+//     { id: 1, _id: 0 },
+//   )
+//     .sort({
+//       createdAt: -1,
+//     })
+//     .lean();
+
+//   return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+// };
+
+// export const generateStudentId = async (payload: TAcademicSemester) => {
+//   const currentId = (await findLastStudenId()) || (0).toString();
+//   let incrementId = (parseFloat(currentId) + 1).toString().padStart(4, '0');
+//   incrementId = `${payload.year}${payload.code}${incrementId}`;
+//   return incrementId;
+// };
+
 import { TAcademicSemester } from '../academicSemister/academicSemester.interface';
 import { User } from './user.model';
 
@@ -13,11 +38,23 @@ const findLastStudenId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudenId()) || (0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudenId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentSemesterYear === currentSemesterYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (parseFloat(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
