fc1dfbb151960b3a3acb95ae781e8f0346d993d2
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-05-31 21:54:19 +0600

    added academicFacultyies all .ts file

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,17 +1,21 @@
 import { z } from 'zod';
 
 const academicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty Must Be String',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty Must Be String',
+    }),
   }),
 });
 
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z
-    .string({
-      invalid_type_error: 'Academic Faculty Must Be String',
-    })
-    .optional(),
+  body: z.object({
+    name: z
+      .string({
+        invalid_type_error: 'Academic Faculty Must Be String',
+      })
+      .optional(),
+  }),
 });
 
 export const academicFacultyValidation = {
