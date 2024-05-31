b3bb97958ab2a7a49cbe470f75e89cafcf80dcb8
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-05-31 23:06:03 +0600

    Create  Interface, Model, Validation, ServiceController, Route and Test Using Postman

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { StudentRoutes } from '../modules/student/student.route';
 import { UserRouter } from '../modules/user/user.route';
 import { AcademicSemisterRouter } from '../modules/academicSemister/academicSemester.route';
 import { AcademicFacultyRoute } from '../modules/academicFaculty/academicFaculty.route';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 
 const router = Router();
 
@@ -11,6 +12,7 @@ const moduleRoutes = [
   { path: '/students', route: StudentRoutes },
   { path: '/academic-semister', route: AcademicSemisterRouter },
   { path: '/academic-faculties', route: AcademicFacultyRoute },
+  { path: '/academic-departments', route: AcademicDepartmentRoutes },
 ];
 // router.use('/users', UserRouter);
 //এখানে map use করা লাগবে না , কারন এখানে আমাদের কোন কিছু return করা লাগবে না , তাই forEach use করা হয়েছে , কারন forEach কোন কিছু return করে না
