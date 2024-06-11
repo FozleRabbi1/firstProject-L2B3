bb1098a69ab1519bfe1c8be9244d11cc370b169a
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-06-11 18:04:48 +0600

    Create An AccessToken with JWT

--- src/app/modules/Auth/Auth.service.ts
+++ src/app/modules/Auth/Auth.service.ts
@@ -2,6 +2,8 @@ import httpStatus from 'http-status';
 import { AppError } from '../../errors/AppErrors';
 import { User } from '../user/user.model';
 import { TLoginUser } from './Auth.interface';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 // import bcrypt from 'bcrypt';
 
 const loginUserService = async (paylod: TLoginUser) => {
@@ -27,19 +29,37 @@ const loginUserService = async (paylod: TLoginUser) => {
   //   throw new AppError(httpStatus.FORBIDDEN, 'password is not matching');
   // }
 
-
-  // ========================================>>>>>>>>>> statices method
+  // ========================================>>>>>>>>>> STATICKS method
   const userData = await User.isUserExistsByCustomeId(paylod.id);
 
   if (!userData) {
     throw new AppError(httpStatus.NOT_FOUND, 'User is not found');
   }
-
+  // =================================>>>>>  checking if the password is correct or not
   if (!(await User.isPasswordMatched(paylod?.password, userData?.password))) {
     throw new AppError(httpStatus.FORBIDDEN, 'password is not matching');
   }
+  // ================================>>>>>  checking if the user is already deleted or not
+  if (userData?.isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is already deleted');
+  }
+  // =================================>>>>>  checking if the user is already Blocked  or in-progress
+  if (userData?.status !== 'in-progress') {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is already Blocked');
+  }
 
-  return {};
+  const jwtPayload = {
+    userId: userData.id,
+    role: userData.role,
+  };
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '10d',
+  });
+
+  return {
+    accessToken,
+    needPasswordChange: userData.needPasswordChange,
+  };
 };
 
 export const UsersLoginService = {
