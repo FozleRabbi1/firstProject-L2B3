31ba5e81cbad943589120b8474a4a6f6cb5ec74b
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-06-13 10:25:12 +0600

    update disst

--- dist/app.js
+++ dist/app.js
@@ -19,11 +19,13 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const notFound_1 = __importDefault(require("./app/middleware/notFound"));
 const routes_1 = __importDefault(require("./app/routes"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const globalErrorHandlear_1 = __importDefault(require("./app/middleware/globalErrorHandlear"));
 const app = (0, express_1.default)();
 //parser
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({ origin: ['http://localhost:5173'] }));
 // application routes
 app.use('/api/v1', routes_1.default);
 app.get('/test', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
